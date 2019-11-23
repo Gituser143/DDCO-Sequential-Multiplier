c83d05d0f88f1bbff3b276b8ea7715405e6a8785
Gituser143 <bhargavsnv100@gmail.com>
2019-11-23 19:48:02 +0530

    Fix shifting

--- tb.v
+++ tb.v
@@ -1,26 +1,24 @@
-
 module tb;
-wire signed [32:0] z;
-reg signed [15:0] a,b;
+reg [32:0] z;
+wire [15:0] a,b;
 
 
-multiplier my_mul(.a(a),.b(b),.c(z));
+multiplier my_mul(a, b, z);
 
 initial begin $dumpfile("tb_mul.vcd"); $dumpvars(0,tb); end
 
 initial
 begin
-$monitor($time,"       ",a," *",b," = ", z);
+$monitor($time,"     ",a," *",b," = ", z);
 
-a = 16'b0000000011110000;
-b = 16'b0000000000000001;
+	a = 16'b0000000011110000;
+	b = 16'b0000000000000001;
 
-#10
+	#10
 
-a = 16'b0000000010010101;
-b = 16'b0000000000000001;
+	a = 16'b0000000010010101;
+	b = 16'b0000000000000001;
 
-#10
-
+	#10
 end
 endmodule
